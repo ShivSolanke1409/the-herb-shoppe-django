f02c80839667deaefb3c3fb3ad5a9334c8cc6587
ShivSolanke1409 <shivsolanke1409@gmail.com>
2026-01-13 13:44:03 +0530

    Fix media files loading on Render

--- herbshoppe/urls.py
+++ herbshoppe/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for herbshoppe project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
@@ -24,12 +8,14 @@ from pages.views import home
 urlpatterns = [
     path('', home, name='home'),
     path('admin/', admin.site.urls),
+
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
+
     path('', include('products.urls')),
     path('cart/', include('cart.urls')),
     path('orders/', include('orders.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ✅ FORCE MEDIA SERVING IN PRODUCTION (RENDER FIX)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
