b7db0ac244ee4e17816ab64a07e43558aeda10f4
ShivSolanke1409 <shivsolanke1409@gmail.com>
2026-01-13 15:22:32 +0530

    Fix Cloudinary env configuration

--- herbshoppe/urls.py
+++ herbshoppe/urls.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from pages.views import home
 
 urlpatterns = [
@@ -16,6 +13,3 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('orders/', include('orders.urls')),
 ]
-
-# ✅ FORCE MEDIA SERVING IN PRODUCTION (RENDER FIX)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
